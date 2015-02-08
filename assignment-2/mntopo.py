9b21b8ad5f073c9ec32626322eedbd9cde192e3c
Jon Erickson <jnericks@gatech.edu>
2015-02-08 15:44:47 -0800

    Saving work

--- assignment-2/mntopo.py
+++ assignment-2/mntopo.py
@@ -14,17 +14,24 @@ class MNTopo(Topo):
 
         # Host and link configuration
         hostConfig = {'cpu': cpu}
-        linkConfig = {'bw': 10, 'delay': '1ms', 'loss': 0,
+        linkConfig = {'bw': 50, 'delay': '10ms', 'loss': 0,
                    'max_queue_size': max_queue_size }
 
         # Hosts and switches
         s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
         sender = self.addHost('sender', **hostConfig)
         receiver = self.addHost('receiver', **hostConfig)
 
-        # Wire receiver
+        # Wire receiver to s1
         self.addLink(receiver, s1, **linkConfig)
 
-        # Wire sender
-        self.addLink(sender, s1, **linkConfig)
+        # Wire s1 to s2
+        self.addLink(s1, s2, **linkConfig)
 
+        # Wire s2 to s3
+        self.addLink(s2, s3, **linkConfig)
+
+        # Wire sender
+        self.addLink(sender, s3, **linkConfig)
